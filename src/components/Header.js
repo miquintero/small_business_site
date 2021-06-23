3b6dfb32072faeae3f5ac1bebd920f1d9c30d419
miquintero <schaeffer.quintero@gmail.com>
2021-06-23 13:38:55 -0400

    style: change main header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
+        <h1>Magaly Schaeffer</h1>
+        <p>Bicultural Bilingual Imago Psychotherapist<br />
         design by <a href="https://html5up.net">HTML5 UP</a>.</p>
     </header>
 )
